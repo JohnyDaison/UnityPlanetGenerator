efa75544a6df7bbaf6022fd593b7d8f44bfcb128
Johny Daison <johnydaison@gmail.com>
2023-07-07 18:59:54 +0200

    rotate the planet slowly

--- Assets/Scripts/AutoRotation.cs
+++ Assets/Scripts/AutoRotation.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class AutoRotation : MonoBehaviour {
 
+    [SerializeField]
+    private float rotationSpeed;
+
+    public float RotationSpeed
+    {
+        get { return rotationSpeed; }
+        set { rotationSpeed = value; }
+    }
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +20,6 @@ public class AutoRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Rotate(new Vector3(0, 0, 1f));
+        this.transform.Rotate(new Vector3(0, 0, this.RotationSpeed));
 	}
 }
